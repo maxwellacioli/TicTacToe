6204f6b9e918b5c99a027f2b2fc6191e4b27b0f4
Maxwell <Maxwell@192.168.25.12>
2017-02-01 00:04:41 -0300

    verificação de vencedor

--- src/main/java/util/Board.java
+++ src/main/java/util/Board.java
@@ -5,12 +5,15 @@ import java.util.Random;
 
 public class Board {
 
+	private Integer starter;
+	private int winner = -1;
 	private ArrayList<Integer> boardCells;
 	private static final Integer EMPTY_CELL = -1;
-	private static final Integer PLAYER = 1;
-	private static final Integer COMPUTER = 0;
+	public static final Integer PLAYER = 1;
+	public static final Integer COMPUTER = 0;
 
-	public void createBoard() {
+	// Cria o array das celulas do jogo
+	private void createBoard() {
 		boardCells = new ArrayList<Integer>();
 
 		for (int i = 0; i < 9; i++) {
@@ -18,33 +21,115 @@ public class Board {
 		}
 	}
 
-	// TODO Remover a impressao do valor vazio (-1)
+	// Imprime o tabuleiro
 	public void printBoard() {
 		int cell = 0;
 
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 2; j++) {
-				if (boardCells.get(cell) >= 0) {
-					System.out.print(boardCells.get(cell) + " |");
+				if (boardCells.get(cell) == 1) {
+					System.out.print("X |");
+				} else if (boardCells.get(cell) == 0) {
+					System.out.print("O |");
 				} else {
 					System.out.print("  |");
 				}
 				cell++;
 			}
-			if (boardCells.get(cell) >= 0) {
-				System.out.print(boardCells.get(cell));
+			if (boardCells.get(cell) == 1) {
+				System.out.print("X");
+			} else if (boardCells.get(cell) == 0) {
+				System.out.print("O");
 			}
 			System.out.println();
 			if (i < 2) {
 				System.out.println("-- -- --");
 			}
+			cell++;
 		}
 	}
 
-	public int getStarterPlayer() {
+	// Define o jogador que irá começar a partida
+	private int getStarterPlayer() {
 		Random rnd = new Random();
 
 		int rndNumber = rnd.nextInt(2);
 		return rndNumber;
+	}
+
+	// Seta a posição jogada pelo ultimo jogador
+	public void setPlay(int player, int position) {
+		boardCells.set(position - 1, player);
+	}
+
+	public Integer getStarter() {
+		return starter;
+	}
+
+	// Inicia os parâmetros do jogo
+	public void initGame() {
+		createBoard();
+
+		starter = getStarterPlayer();
+	}
+
+	// TODO
+	public void computerDefense() {
+
+	}
+
+	// TODO melhorar implementação
+	public boolean hasWinner() {
+
+		// verificação linha 1
+		if (verifyWinnerLine(boardCells.get(0), boardCells.get(1), boardCells.get(2))) {
+			winner = boardCells.get(0);
+		}
+		// verificação linha 2
+		else if (verifyWinnerLine(boardCells.get(3), boardCells.get(4), boardCells.get(5))) {
+			winner = boardCells.get(3);
+		}
+		// verificação linha 3
+		else if (verifyWinnerLine(boardCells.get(6), boardCells.get(7), boardCells.get(8))) {
+			winner = boardCells.get(6);
+		}
+		// verificação coluna 1
+		else if (verifyWinnerLine(boardCells.get(0), boardCells.get(3), boardCells.get(6))) {
+			winner = boardCells.get(0);
+		}
+		// verificação coluna 2
+		else if (verifyWinnerLine(boardCells.get(1), boardCells.get(4), boardCells.get(7))) {
+			winner = boardCells.get(1);
+		}
+		// verificação coluna 3
+		else if (verifyWinnerLine(boardCells.get(2), boardCells.get(5), boardCells.get(8))) {
+			winner = boardCells.get(2);
+		}
+		// verificação diagonal 1
+		else if (verifyWinnerLine(boardCells.get(0), boardCells.get(4), boardCells.get(8))) {
+			winner = boardCells.get(0);
+		}
+		// verificação diagonal 2
+		else if (verifyWinnerLine(boardCells.get(2), boardCells.get(4), boardCells.get(6))) {
+			winner = boardCells.get(2);
+		}
+
+		if(winner >= 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// Verifica se existe uma reta com os três números iguais
+	private boolean verifyWinnerLine(int cell1, int cell2, int cell3) {
+		if (cell1 == cell2 && cell2 == cell3)
+			return true;
+		else
+			return false;
+	}
+
+	public int getWinner() {
+		return winner;
 	}
 }
